0ac20a00be8b7c23f985143993b76f3986daf107
Patric Forsgard <patric@tasteful.se>
2016-02-11 10:17:46 +0100

    Test: Where_subquery_expression_same_parametername

--- test/EntityFramework.Core.FunctionalTests/QueryTestBase.cs
+++ test/EntityFramework.Core.FunctionalTests/QueryTestBase.cs
@@ -1185,6 +1185,17 @@ namespace Microsoft.Data.Entity.FunctionalTests
         }
 
         [Fact]
+        public virtual void Where_subquery_expression_same_parametername()
+        {
+            AssertQuery<Order, Order>((o1, o2) =>
+            {
+                var firstOrder = o1.First();
+                Expression<Func<Order, bool>> expr = x => x.OrderID == firstOrder.OrderID;
+                return o1.Where(x => o2.Where(expr).Where(o => o.CustomerID == x.CustomerID).Any());
+            });
+        }
+
+        [Fact]
         public virtual void Where_bool_member()
         {
             AssertQuery<Product>(ps => ps.Where(p => p.Discontinued), entryCount: 8);
